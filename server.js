5bbade15fab726eef2d6ab9961e1e585b3ad600b
amalvinoth <42750494+amalvinoth@users.noreply.github.com>
2018-08-30 00:37:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ mongoose.connect(dbConfig.url, {
 }).then(() => {
     console.log("Successfully connected to the database");    
 }).catch(err => {
-    console.log('Could not connect to the database. Exiting now...');
+    console.log(err,'Could not connect to the database. Exiting now...');
     process.exit();
 });
 
